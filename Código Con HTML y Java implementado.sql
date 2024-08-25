02bc1c79b3df6c8c0c63a803c99c83aca01453c9
O. Anderson Neuto González <165607850+Anderzonneuto@users.noreply.github.com>
2024-08-25 13:42:27 -0500

    Add files via upload

---
+++ Código Con HTML y Java implementado.sql
@@ -0,0 +1,49 @@
+-- Active: 1711925839605@@127.0.0.1@3306
+CREATE DATABASE , proyectofotomago
+    DEFAULT CHARACTER SET = 'utf8mb4';
+    ass="container center-block" >
+v class="row justify-content-center">
+
+<div class="card" style="width:18rem;">
+<img scr="logo.png" class="card-img-top" alt="...">
+<div class="card-body">
+
+<form method="POST" action=Validar">
+    <div class="form-group">
+         <label for="documento">Nº de Documento</label>
+         <input type="text" class="form-control" name="txtusuario">
+         <small id="documento" class="form-text text muted">Ingrese su Documento, sin espacios, mi puntos">
+    </div> 
+    <div class="form-group">
+        <label for="exampleInputpassword">Contraseña</label>
+        <input type="password"class="form-control"id=exampleInputpassword">
+    </div> 
+    <div class="form-group form-check">
+        <input type="checkbox" class="form-check-input" id="exampleCheckl">
+        <label class="form-check-label"exampleCheckl">Permanecer Loggueado</label>
+        <div/>
+        <button type="submit"class="btn btn-primary" name="accion" value=ingresar">Ingresa</button>
+                     </div>
+                     <div class="form-group">
+                        <label for="exampleInputpasswordl">Contraseña</label
+                        <input type="password" class="form-control" id=exampleInputpasswordl">
+                    </div>
+                    <div class="form-group form check">
+                        <input type="checkbox" class="form-check-input" id="example-Checkl">
+                        <label class="form-check-label" for="exampleCheckl">permsanecer loggeado</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="accion" value="ingresar">ingresar"</button>
+                </form>
+            </div>
+         </div>
+    </div>
+</div>
+
+--optional JavaScrip; choose one of the two! -->
+--option 1:jQuery Bootstrap bundle (includes Popeer) -->
+cript scr="https://code,jquery.com/jquery-3.5.1.slim,min,js" integrity=sha384-DfXdz2htpPHO1sSSs5nctpuj/zyc+
+cript scr="https://cdn.jsdelivr.net/npm/bootstrap4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-h
+    
+    option 2: jQuery, Popper.js, and Boostrap JS
+    cript src="https://code.jquery.com/jquery-3.5.1"slim,min,js" integrity=sha384-DfXdz2htpPHO1sSSs5nctpuj/zyc+
+    
